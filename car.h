2a62358646e29d88c07d74cb834df6741c01dc0c
jfischlmayr <jan.fischlmayr@gmx.net>
2018-12-18 16:02:56 +0100

    Final Program

--- car.h
+++ car.h
@@ -13,4 +13,18 @@
 #ifndef ___CAR_H
 #define ___CAR_H
 
+enum CarType { AIXAM, FIAT_MULTIPLA, JEEP };
+enum Color { RED, BLACK, BLUE, ORANGE, SILVER, GREEN};
+
+typedef struct CarImplementation* Car;
+
+int get_speed(Car car);
+double get_fill_level(Car car);
+double get_acceleration_rate(Car car);
+void init();
+void set_acceleration_rate(Car car, double acceleration_rate);
+void accelerate(Car car);
+Car get_car(enum CarType type);
+enum CarType get_type(Car car);
+enum Color get_color(Car car);
 #endif
